4858041dca9901758c8abda85f1ab33876d9c832
Project_IO <me@projecttl.net>
2023-03-27 14:31:42 +0900

    feat: First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+
+    application
+}
+
+group = "net.projecttl"
+version = "1.0.0"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+
+        options.encoding = "UTF-8"
+    }
+
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveClassifier.set("")
+        archiveVersion.set("")
+
+        manifest {
+            attributes["Main-Class"] = "net.projecttl.example.Main"
+        }
+    }
+
+    getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+}
+
+application {
+    mainClass.set("net.projecttl.example.Main")
+}
